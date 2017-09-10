bcaafc56bfe9abb878c0a380c883c821577a2a88
JoeSera <31440396+JoeSera@users.noreply.github.com>
2017-09-10 22:08:46 +0700

    Complete project Minesweeper: Dynamically Generate Game Boards DAY 10,11

--- Desktop/Projects/minesweeper/src/minesweeper.js
+++ Desktop/Projects/minesweeper/src/minesweeper.js
@@ -1,3 +1,4 @@
+// Players board
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   let board = [];
   for (let i = 0; i<numberOfRows; i++){
@@ -10,4 +11,47 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return board;
 };
 
-console.log(generatePlayerBoard(2,3));
+//--------------------------------------------------------------
+
+// bomb board
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let i = 0; i<numberOfRows; i++){
+    let row = [];
+    for (let j= 0; j<numberOfColumns; j++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  // Placing bombs
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs){
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfRows);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    // possible that bomb will be placed on the same spot
+  }
+  return board;
+};
+
+//--------------------------------------------------------------
+
+// Print Board
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+//--------------------------------------------------------------
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+// Print Players Board
+console.log('Player Board: ');
+printBoard(playerBoard);
+
+// Print Bomb Board
+console.log('Bomb Board: ');
+printBoard(bombBoard);
